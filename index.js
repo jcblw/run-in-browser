ce85502ab05bf9d57251340ee6956e3c394c20d1
Jacob Lowe <jacoblowe2.0@gmail.com>
2014-12-21 13:50:03 -0800

    Revising the styling of the script and getting launcher and server module working

--- index.js
+++ index.js
@@ -1,40 +1,62 @@
-var browserify = require( 'browserify' ),
-    merge = require( 'merge' ),
-    EventEmitter2 = require('eventemitter2').EventEmitter2,
-    server = require( './src/server' );
+var 
+browserify = require( 'browserify' ),
+merge = require( 'merge' ),
+EventEmitter2 = require('eventemitter2').EventEmitter2,
+server = require( './src/server' ),
+exposify = require( 'exposify' ),
+launcher = require( './src/launcher' )
+
+exposify.config = { server: '_SERVER_' }
 
 module.exports = RunInBrowser
 
 function RunInBrowser( opts ) {
     
-    var options = merge( opts, { emitter : this } ),
-        setup = server( opts );
+    var 
+    options = merge( opts, { emitter : this } ),
+    setup = server( opts )
 
-    this._sock = setup.sock;
-    this.port = setup.port;
-    this.server = setup.server;
-    this.scripts = [];
+    this._sock = setup.sock
+    this.port = setup.port
+    this.server = setup.server
+    this.scripts = []
     EventEmitter2.call( this, opts.events )
-    this.on( 'connection', this._runScripts.bind( this ) );
+    setImmediate( this._init.bind( this, options ) );
 }
 
-RunInBrowser.prototype = Object.create( EventEmitter2.prototype );
+RunInBrowser.prototype = Object.create( EventEmitter2.prototype )
 
 RunInBrowser.prototype.require = function( entry ) {
-    var error = this.emit.bind( this, true, 'error' ),
-        pushScript = this.scripts.push.bind( this.scripts )
 
-    browserify( process.cwd() + entry )
-        //.require( './client/server.js', { expose: 'server' } )
-        // need to revise this to access global var
-        .bundle( function( err, buf ) {
-            if ( err ) return error( err )
-            pushScript( buf.toString( 'utf8' ) )
-        } )
+    browserify( process.cwd() + '/' + entry )
+        .transform( exposify )
+        .bundle( this._onBundle.bind( this ) )
 }
 
-RunInBrowser.prototype._runScripts = function ( ) {
-    console.log( 'new connection' );
-    runCode = this.emit.bind( this, 'RUNCODE' );
-    this.scripts.forEach( runCode );
+RunInBrowser.prototype._init = function( opts ) {
+
+    this.on( 'connection', this._runScripts.bind( this ) )
+    launcher( opts, this._onLaunch.bind( this ) )
+}
+
+RunInBrowser.prototype._onBundle = function( err, buf ) {
+
+    if ( err ) return this.emit( true, 'error', err )
+    this.scripts.push( buf.toString( 'utf8' ) )
+    if ( this.browser ) {
+        this._runScripts( )
+    }
+}
+
+RunInBrowser.prototype._onLaunch = function( err, browser ) {
+
+    if ( err ) return this.emit( true, 'error', err )
+    this.browser = browser.process
+}
+
+RunInBrowser.prototype._runScripts = function( ) {
+
+    var
+    runCode = this.emit.bind( this, 'RUNCODE' )
+    this.scripts.forEach( runCode )
 }
--- src/server.js
+++ src/server.js
@@ -1,23 +1,28 @@
 
-var http = require( 'http' ),
-    shoe = require( 'shoe' ),
-    utils = require( '../shared/utils' ),
-    through = require( 'through' ),
-    ecstatic = require( 'ecstatic' )( __dirname + '/../client' )
+var 
+http = require( 'http' ),
+shoe = require( 'shoe' ),
+utils = require( '../shared/utils' ),
+through = require( 'through' ),
+ecstatic = require( 'ecstatic' )( __dirname + '/../client' )
 
 module.exports = function( opts ) {
-    var emitter = opts.emitter,
-        server = http.createServer( ecstatic ),
-        port = opts.port || 9090,
-        sock = shoe( handleStream ),
-        ogEmit = emitter.emit;
+    var 
+    emitter = opts.emitter,
+    server = http.createServer( ecstatic ),
+    port = opts.port || 9090,
+    sock = shoe( handleStream ),
+    ogEmit = emitter.emit;
 
     function handleStream( stream ) {
         
         stream.pipe( through( eventTransform ) );
 
         emitter.emit = function ( ) {
-            var args = utils.makeArray( arguments )
+            
+            var 
+            args = utils.makeArray( arguments )
+
             if ( typeof args[ 0 ] === 'string' ) {
                 stream.write( JSON.stringify( args ) ); // this sends an event to the server
                 return
@@ -30,7 +35,9 @@ module.exports = function( opts ) {
     }
 
     function eventTransform( msg ) {
-        var data = utils.deserialize( msg )
+        
+        var 
+        data = utils.deserialize( msg )
         if ( utils.isEvent( data ) ) {
             data.unshift( true ) // added true to first param to send data out from normal emitter
             emitter.emit.apply( emitter, data )
